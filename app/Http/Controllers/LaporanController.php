b98aa5a2ecbf1e4cae275273e835e233a1df9b4d
Agus Muhamad Gaostul Ibad <gaostulibad@gmail.com>
2023-06-08 16:35:45 +0700

    update laporan

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class LaporanController extends Controller
 {
    public function laporan()
    {
         $data['title'] = 'Laporan Penjualan';
 
+        $laporan = DB::table('pemesanan')
+            ->join('barang','barang.id','=','pemesanan.id_barang')
+            ->select('pemesanan.*','barang.nama_barang')
+            ->where('pemesanan.status_pemesanan','=','Selesai')
+            ->get();
         
-        return view('admin.laporan', $data);
+        return view('admin.laporan',compact('laporan'), $data);
    }
 }
--- resources/views/admin/laporan.blade.php
+++ resources/views/admin/laporan.blade.php
@@ -36,77 +36,17 @@
                     <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 202.48px;">Name</th><th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 322.48px;">Position</th><th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 149.102px;">Office</th><th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 61.1719px;">Age</th><th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 130.742px;">Start date</th><th class="sorting" tabindex="0" aria-controls="zero_config" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 127.773px;">Salary</th></tr>
                   </thead>
                   <tbody>
-                  <tr role="row" class="odd">
-                      <td class="sorting_1">Airi Satou</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>33</td>
-                      <td>2008/11/28</td>
-                      <td>$162,700</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Angelica Ramos</td>
-                      <td>Chief Executive Officer (CEO)</td>
-                      <td>London</td>
-                      <td>47</td>
-                      <td>2009/10/09</td>
-                      <td>$1,200,000</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Ashton Cox</td>
-                      <td>Junior Technical Author</td>
-                      <td>San Francisco</td>
-                      <td>66</td>
-                      <td>2009/01/12</td>
-                      <td>$86,000</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Bradley Greer</td>
-                      <td>Software Engineer</td>
-                      <td>London</td>
-                      <td>41</td>
-                      <td>2012/10/13</td>
-                      <td>$132,000</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Brenden Wagner</td>
-                      <td>Software Engineer</td>
-                      <td>San Francisco</td>
-                      <td>28</td>
-                      <td>2011/06/07</td>
-                      <td>$206,850</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Brielle Williamson</td>
-                      <td>Integration Specialist</td>
-                      <td>New York</td>
-                      <td>61</td>
-                      <td>2012/12/02</td>
-                      <td>$372,000</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Bruno Nash</td>
-                      <td>Software Engineer</td>
-                      <td>London</td>
-                      <td>38</td>
-                      <td>2011/05/03</td>
-                      <td>$163,500</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Caesar Vance</td>
-                      <td>Pre-Sales Support</td>
-                      <td>New York</td>
-                      <td>21</td>
-                      <td>2011/12/12</td>
-                      <td>$106,450</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Cara Stevens</td>
-                      <td>Sales Assistant</td>
-                      <td>New York</td>
-                      <td>46</td>
-                      <td>2011/12/06</td>
-                      <td>$145,600</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Cedric Kelly</td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td>$433,060</td>
-                    </tr></tbody>
+                    @foreach ($laporan as $item)
+                        <tr role="row" class="odd">
+                          <td class="sorting_1">{{ $item->nama_barang }}</td>
+                          <td>{{ $item->jumlah_berat }} Ton</td>
+                          <td>{{ $item->status_pemesanan }}</td>
+                          <td>{{ $item->total_harga }}</td>
+                          <td>{{ date_format(date_create($item->created_at),'d-M-Y') }}</td>
+                          <td>$162,700</td>
+                        </tr> 
+                    @endforeach
+                   </tbody>
                   <tfoot>
                     <tr><th rowspan="1" colspan="1">Name</th><th rowspan="1" colspan="1">Position</th><th rowspan="1" colspan="1">Office</th><th rowspan="1" colspan="1">Age</th><th rowspan="1" colspan="1">Start date</th><th rowspan="1" colspan="1">Salary</th></tr>
                   </tfoot>
